d85aca84d206446d3128255e5c495446a67bfe13
langua <lori3f6@gmail.com>
2024-10-19 16:27:36 +0200

    add accessibility theme for bird ui

--- themes/bird-ui.sh
+++ themes/bird-ui.sh
@@ -1,3 +1,5 @@
+echo "Setting up bird ui..."
+
 # ref: https://github.com/ronilaukkarinen/mastodon-bird-ui
 
 export MASTODON_VERSION_FOR_BIRD_UI="main"
@@ -32,14 +34,22 @@ echo -e "@import 'mastodon-light/variables';\n@import 'application';\n@import 'm
 # Create dark theme file
 echo -e "@import 'application';\n@import 'mastodon-bird-ui/layout-single-column.scss';\n@import 'mastodon-bird-ui/layout-multiple-columns.scss';" > mastodon_source/app/javascript/styles/mastodon-bird-ui-dark.scss
 
+# Add ultra accessable theme
+# ref: https://github.com/ronilaukkarinen/mastodon-bird-ui?tab=readme-ov-file#accessibility
+cp -v mastodon_source/app/javascript/styles/mastodon-bird-ui-contrast.scss mastodon_source/app/javascript/styles/mastodon-bird-ui-accessible.scss
+cat assets/mastodon-bird-ui-accessible.scss >> mastodon_source/app/javascript/styles/mastodon-bird-ui-accessible.scss
+
 # Write config into themes.yml
-echo -e "mastodon-bird-ui-dark: styles/mastodon-bird-ui-dark.scss\nmastodon-bird-ui-light: styles/mastodon-bird-ui-light.scss\nmastodon-bird-ui-contrast: styles/mastodon-bird-ui-contrast.scss" >> mastodon_source/config/themes.yml
+echo "mastodon-bird-ui-dark: styles/mastodon-bird-ui-dark.scss" >> mastodon_source/config/themes.yml
+echo "mastodon-bird-ui-light: styles/mastodon-bird-ui-light.scss" >> mastodon_source/config/themes.yml
+echo "mastodon-bird-ui-contrast: styles/mastodon-bird-ui-contrast.scss" >> mastodon_source/config/themes.yml
+echo "mastodon-bird-ui-accessible: styles/mastodon-bird-ui-accessible.scss" >> mastodon_source/config/themes.yml
 
 # Add i18n 
 # ref: https://github.com/wxwmoe/mastodon
-sed -i '/mastodon-light/a\    mastodon-bird-ui-dark: Mastodon Bird UI (Dark)\n    mastodon-bird-ui-contrast: Mastodon Bird UI (High contrast)\n    mastodon-bird-ui-light: Mastodon Bird UI (Light)' mastodon_source/config/locales/en.yml 
-sed -i '/mastodon-light/a\    mastodon-bird-ui-dark: Mastodon Bird UI（暗色主题）\n    mastodon-bird-ui-contrast: Mastodon Bird UI（高对比度）\n    mastodon-bird-ui-light: Mastodon Bird UI（亮色主题）' mastodon_source/config/locales/zh-CN.yml 
-sed -i '/mastodon-light/a\    mastodon-bird-ui-dark: Mastodon Bird UI（深色）\n    mastodon-bird-ui-contrast: Mastodon Bird UI（高對比）\n    mastodon-bird-ui-light: Mastodon Bird UI（亮色）' mastodon_source/config/locales/zh-TW.yml 
-sed -i '/mastodon-light/a\    mastodon-bird-ui-dark: Mastodon Bird UI\n    mastodon-bird-ui-contrast: Mastodon Bird UI（高對比）\n    mastodon-bird-ui-light: Mastodon Bird UI（亮色主題）' mastodon_source/config/locales/zh-HK.yml 
-sed -i '/mastodon-light/a\    mastodon-bird-ui-dark: Mastodon Bird UI (ダーク)\n    mastodon-bird-ui-contrast: Mastodon Bird UI (ハイコントラスト)\n    mastodon-bird-ui-light: Mastodon Bird UI (ライト)' mastodon_source/config/locales/ja.yml 
+sed -i '/mastodon-light/a\    mastodon-bird-ui-dark: Mastodon Bird UI (Dark)\n    mastodon-bird-ui-contrast: Mastodon Bird UI (High contrast)\n    mastodon-bird-ui-light: Mastodon Bird UI (Light)\n    mastodon-bird-ui-accessible: Mastodon Bird UI (Accessible)' mastodon_source/config/locales/en.yml 
+sed -i '/mastodon-light/a\    mastodon-bird-ui-dark: Mastodon Bird UI（暗色）\n    mastodon-bird-ui-contrast: Mastodon Bird UI（高对比度）\n    mastodon-bird-ui-light: Mastodon Bird UI（亮色）\n    mastodon-bird-ui-accessible: Mastodon Bird UI（无障碍）' mastodon_source/config/locales/zh-CN.yml 
+sed -i '/mastodon-light/a\    mastodon-bird-ui-dark: Mastodon Bird UI（深色）\n    mastodon-bird-ui-contrast: Mastodon Bird UI（高對比）\n    mastodon-bird-ui-light: Mastodon Bird UI（亮色）\n    mastodon-bird-ui-accessible: Mastodon Bird UI（無障礙）' mastodon_source/config/locales/zh-TW.yml 
+sed -i '/mastodon-light/a\    mastodon-bird-ui-dark: Mastodon Bird UI（深色）\n    mastodon-bird-ui-contrast: Mastodon Bird UI（高對比）\n    mastodon-bird-ui-light: Mastodon Bird UI（亮色）\n    mastodon-bird-ui-accessible: Mastodon Bird UI（無障礙）' mastodon_source/config/locales/zh-HK.yml 
+sed -i '/mastodon-light/a\    mastodon-bird-ui-dark: Mastodon Bird UI (ダーク)\n    mastodon-bird-ui-contrast: Mastodon Bird UI (ハイコントラスト)\n    mastodon-bird-ui-light: Mastodon Bird UI (ライト)\n    mastodon-bird-ui-accessible: Mastodon Bird UI (アクセシブル)' mastodon_source/config/locales/ja.yml 
 
